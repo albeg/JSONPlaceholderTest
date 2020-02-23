e2ffb5890d8e26955ad30e22d52fe5fce22707e2
Dmitry <dvpris06@gmail.com>
2020-02-23 15:42:53 +0300

    Add message for empty table

--- JSONPlaceholderTest/Controller/UsersViewController.swift
+++ JSONPlaceholderTest/Controller/UsersViewController.swift
@@ -59,6 +59,12 @@ extension UsersViewController: UITableViewDelegate, UITableViewDataSource {
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        if users.count == 0 {
+            self.tableView.setEmptyMessage("Users will be loaded soon!")
+        } else {
+            self.tableView.restore()
+        }
+
         return users.count
     }
     
@@ -102,6 +108,27 @@ extension UsersViewController: UITableViewDelegate, UITableViewDataSource {
         tableView.deselectRow(at: indexPath, animated: true)
     }
     
+}
+
+extension UITableView {
+
+    func setEmptyMessage(_ message: String) {
+        let messageLabel = UILabel(frame: CGRect(x: 0, y: 0, width: self.bounds.size.width, height: self.bounds.size.height))
+        messageLabel.text = message
+        messageLabel.textColor = .black
+        messageLabel.numberOfLines = 0
+        messageLabel.textAlignment = .center
+        messageLabel.font = UIFont(name: "TrebuchetMS", size: 25)
+        messageLabel.sizeToFit()
+
+        self.backgroundView = messageLabel
+        self.separatorStyle = .none
+    }
+
+    func restore() {
+        self.backgroundView = nil
+        self.separatorStyle = .singleLine
+    }
 }
 
 
